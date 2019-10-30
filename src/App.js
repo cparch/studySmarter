5a922c0555cb1c1d6ceedb303836d524fe800fa2
Chad Archila <chadparchila@gmail.com>
2019-10-30 09:57:58 -0700

    convert App.js to a Class

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import logo from './logo.svg';
 
-
 import './App.css';
 import Nav from './Components/Nav.js';
 import Home from './Components/Home/Home.js';
@@ -10,33 +9,39 @@ import AddClass from './Components/AddClass.js';
 import AddStudySession from './Components/AddStudySession.js';
 import AddTest from './Components/AddTest.js';
 
-function App() {
-  return (
-    <BrowserRouter>
-      <div>
-        <Nav/>
-        <Switch>
-          <Route 
-            exact
-            path='/' 
-            render={(props) => <Home/>}
-          />
-          <Route 
-            path='/addstudysession' 
-            render={(props) => <AddStudySession />}
-          />
-          <Route
-            path='/addclass'
-            render={(props) => <AddClass {...props}/>}
-          />
-          <Route
-            path='/addtest'
-            render={(props) => <AddTest/>}
-          />
-        </Switch>
-      </div>
-    </BrowserRouter>
-  );
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state={}
+  }
+  render(){
+    return (
+      <BrowserRouter>
+        <div>
+          <Nav/>
+          <Switch>
+            <Route 
+              exact
+              path='/' 
+              render={(props) => <Home/>}
+            />
+            <Route 
+              path='/addstudysession' 
+              render={(props) => <AddStudySession />}
+            />
+            <Route
+              path='/addclass'
+              render={(props) => <AddClass {...props}/>}
+            />
+            <Route
+              path='/addtest'
+              render={(props) => <AddTest/>}
+            />
+          </Switch>
+        </div>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;

462da7dcc8f5ad234b88b4b7b5c8f407f6bba3fc
Chad Archila <chadparchila@gmail.com>
2019-11-13 16:33:57 -0800

    Create funcion to populate an array with avg study times per grade

--- src/Components/StudyInsights/StudyInsights.js
+++ src/Components/StudyInsights/StudyInsights.js
@@ -1,46 +1,134 @@
 import React from 'react';
 import './StudyInsights.css';
 
-const StudyInsights = (props) => {
-  return(
-    <div>
-      <h1>
-        Study Insights
-      </h1>
+class StudyInsights extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      classes: props.classes,
+      avgTimesPerGrade: {},
+      avgTimesPerGradeArray: [["A", "no Data"], ["B", "no Data"], ["C", "no Data"], ["D", "no Data"], ["F", "no Data"]]
+    }
 
-      <h3>
-        This page is intended to give you an idea of what study habbits give you the best chance of earning a grade.
-      </h3>
+    this.FindAvgStudyTimePerGrade = this.FindAvgStudyTimePerGrade.bind(this);
+    this.fromObjToArray = this.fromObjToArray.bind(this);
+  }
 
-      <table id="myTable">
-        <thead>
-          <tr>
-            <th className='cell tableHeader'>Grade</th>
-            <th className='tableHeader'>Average Study Time </th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td className='cell'>A</td>
-            <td>6 hours</td>
-          </tr>
-          <tr>
-            <td className='cell'>B</td>
-            <td>4 hours</td>
-          </tr>
-        </tbody>
-      </table>
-      {/* <div>
-        A: 20 hr
-       
-      </div>
+  FindAvgStudyTimePerGrade(listOfClasses){
+    let totalPerGrade = this.state.avgTimesPerGrade;
 
-      <div>
-        B: 15 hr
-      </div> */}
-    </div>
+    listOfClasses.map( course => {
+      course.test.map(test => {
+        if(test.grade.length > 0){
+
+          if(totalPerGrade[test.grade]){
+            totalPerGrade[test.grade].count += 1;
+            totalPerGrade[test.grade].totalStudyTimePerTest.push(test.totalTimeStudiedPerTest)
+          } else {
+            totalPerGrade[test.grade] = {
+              count: 1,
+              totalStudyTimePerTest: [test.totalTimeStudiedPerTest]
+            }
+          }
+        }
+      })
+    })
+
+    for(let key in totalPerGrade){
+      let timeInMin = 0;
+      totalPerGrade[key].totalStudyTimePerTest.map( time => {
+
+        let timeSplit= time.split(':')
+        let hoursToMin = Number(timeSplit[0]) * 60
+        let hoursPlusMin = hoursToMin + Number(timeSplit[1])
+  
+        timeInMin += hoursPlusMin 
+      })
+      let avgMin = timeInMin/totalPerGrade[key].count
+
+      let totalMinToHr = avgMin/60
+      //330 min / 60 min = 5.5 hr
+
+      if(totalMinToHr.toString().indexOf('.') === -1){
+        //if there is no decimal aka the total min is evenly divisable by 60 min. e.g. 300 min / 60 min = 5 hr
+        totalPerGrade[key].avgTime = totalMinToHr.toString() + ':00'
+
+      } else {
+        //if there is a decimal when you divde the total min by 60 (e.g 332 min / 60 min = 5.5333 hr)
+        let totalMinToHrSplit = totalMinToHr.toString().split('.')
+        // split to [5, 5333]
+
+        let num = Number('.' + totalMinToHrSplit[1]).toFixed(2).split('.')
+
+        num[1] = (num[1] * .60).toFixed(0).toString()
+        //.53 * .60 = 
+
+        if(num[1].toString().length === 1){
+          num[1] = '0' + num[1].toString()
+        }
+
+        totalMinToHrSplit[1] = num[1]
+        //[5, 53]
+
+        totalPerGrade[key].avgTime = totalMinToHrSplit.join(':')
+        //return 5:53
+      }
+    }
+    this.setState({avgTimesPerGrade: totalPerGrade})
+    this.fromObjToArray(this.state.avgTimesPerGrade)
+  }
+
+  fromObjToArray(obj){
+    let updateAvgTimesPerGradeArray = [...this.state.avgTimesPerGradeArray];
+
+    for(let key in this.state.avgTimesPerGrade){
+      updateAvgTimesPerGradeArray.map((grade, idx) => {
     
-  )
+        if(grade[0] === key){
+          updateAvgTimesPerGradeArray[idx][1] = `${this.state.avgTimesPerGrade[key].avgTime} hours`
+        }
+      })
+    }
+  }
+
+  componentDidMount() {
+    this.FindAvgStudyTimePerGrade(this.state.classes)
+  }
+  render(){
+
+    let list = this.state.avgTimesPerGradeArray.map(grade => {
+      return(
+        <tr key={grade[0]}>
+          <td className='cell'>{grade[0]}</td>
+          <td>{grade[1]}</td>
+        </tr>
+      )
+    })
+
+    return(
+      <div>
+        <h1>
+          Study Insights
+        </h1>
+  
+        <h3>
+          This page is intended to give you an idea of what study habbits give you the best chance of earning a grade.
+        </h3>
+  
+        <table id="myTable">
+          <thead>
+            <tr>
+              <th className='cell tableHeader'>Grade</th>
+              <th className='tableHeader'>Average Study Time </th>
+            </tr>
+          </thead>
+          <tbody>
+            {list}
+          </tbody>
+        </table>
+      </div>
+    )
+  }
 };
 
 export default StudyInsights;

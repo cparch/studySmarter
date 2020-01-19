a661ddf6bff7fa6818a1271bcb991369af34e8e3
Chad Archila <chadparchila@gmail.com>
2020-01-19 13:19:38 -0800

    Add controller funcs for addClass, addNewTest, addNewStudySession, addNewTestGrade

--- db/controller/studySmaterController.js
+++ db/controller/studySmaterController.js
@@ -9,3 +9,51 @@ exports.getAllData = (req, res) => {
     res.send(success)
   })
 }
+
+exports.addClass = (req, res) => {
+  gradeInfo.add(req.body, (err, success) => {
+    if(err){
+      res.status(500).end()
+      console.log("Error addClass: ", err)
+    } else {
+      res.status(200).end();
+      console.log("New Class Added to DB")
+    }
+  })
+}
+
+exports.addNewTest = (req, res) => {
+  gradeInfo.addNewTest(req.body, (err, success) => {
+    if(err){
+      res.send(500).end();
+      console.log("Error addNewTest: ", err);
+    } else {
+      res.status(200).end();
+      console.log("New Test Added to DB")
+    }
+  })
+}
+
+exports.addNewStudySession = (req, res) => {
+  gradeInfo.addNewStudySession(req.body, (err, success) => {
+    if(err){
+      res.send(500).end();
+      console.log("Error addNewStudySession: ", err)
+    } else {
+      res.status(200).end();
+      console.log("New Study Session Added to DB")
+    }
+  })
+}
+
+exports.addNewTestGrade = (req, res) => {
+  gradeInfo.addNewTestGrade(req.body, (err, success) => {
+    if(err){
+      res.send(500).end();
+      console.log("Error addNewTestGrade: ", err);
+    } else {
+      res.status(200).end()
+      console.log("New Grade Added to DB")
+    }
+  })
+}

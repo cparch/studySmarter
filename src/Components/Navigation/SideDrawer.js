3ee6f9a860772324968643c7aef5a2b25d62d4ed
Chad Archila <chadparchila@gmail.com>
2020-01-25 15:20:53 -0800

    Add hamburger icon

--- src/Components/Navigation/SideDrawer.js
+++ src/Components/Navigation/SideDrawer.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './SideDrawer.css';
 import Nav from './Nav.js'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
 
 class SideDrawer extends React.Component {
   constructor(props){
@@ -24,7 +26,11 @@ class SideDrawer extends React.Component {
     }
     return(
       <div >
-        <div className='menuBtn' onClick={this.toggleSideDrawerHandler}>Menu</div>
+        <div className='menuBtn' onClick={this.toggleSideDrawerHandler}>
+          <div className='menuBtnContainer'>
+            <FontAwesomeIcon icon={faBars} color="black" size="lg" />
+          </div>
+        </div>
         <div 
           className={toggleSideDrawer}
           onClick={this.toggleSideDrawerHandler}

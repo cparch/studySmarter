86b19969450f0db79d268529d89e6720bd74844a
Chad Archila <chadparchila@gmail.com>
2020-06-11 08:30:36 -0700

    Set up component for redux

--- src/Components/StudySession/AddStudySession.js
+++ src/Components/StudySession/AddStudySession.js
@@ -2,8 +2,16 @@ import React from 'react';
 import './AddStudySession.css';
 import TimeSelector from './TimeSelector.js';
 import 'rc-time-picker/assets/index.css';
+import {useDispatch, useSelector} from 'react-redux'
+import {formHandler, studySessionStartTime, startTimeHandler} from '../../actions/index.js'
 
 const AddStudySession = (props) => {
+
+  const dispatch = useDispatch()
+  const notesValue = useSelector(state => state.FormHandlerReducer.notes)
+  let startTimeValue = useSelector(state => state.FormHandlerReducer.SelectedStartTimeValueToDisplay)
+
+
   return(
     <div>
       <div className='AddStudySessionHeaderImage'></div>
@@ -11,7 +19,9 @@ const AddStudySession = (props) => {
         <h1>Add Study Session</h1>
         <form onSubmit={props.addStudySessionSubmitBtnHandler}>
           <label>
-            <select name="selectedClass" onChange={props.FormHandler}> 
+            {/* <select name="selectedClass" onChange={props.FormHandler}>  */}
+            <select name="selectedClass" onChange={(event) => dispatch(formHandler(event))}> 
+            {/* <select name="selectedClass" onChange={() => console.log("test 10:36")}>  */}
             <option>Class Name:</option> 
               {props.entireState.classes.map((classes, idx) => (
                 <option  key={classes.classTitle} value={idx}>
@@ -22,7 +32,10 @@ const AddStudySession = (props) => {
           </label>
           <br></br>
           <label>
-            <select name="selectedTest" onChange={props.FormHandler}> 
+            {/* <select name="selectedTest" onChange={props.FormHandler}>  */}
+            <select name="selectedTest" onChange={(event) => dispatch(formHandler(event))}> 
+
+
             <option>Test Name:</option> 
                 {props.tests.map((test, idx) => (
                   <option key={test.testName} value={idx}>
@@ -37,15 +50,22 @@ const AddStudySession = (props) => {
             <label >
               Start:
               <TimeSelector
+                id = "startTimePicker"
                 TimeValue={props.startTimeValue}
                 timeHandler={props.handleStudySessionStartTime}
+                //V redux attempt
+                // TimeValue={startTimeValue}
+                // timeHandler = {(value) => dispatch(studySessionStartTime(value))}
+                // timeHandler = {(value) => dispatch(startTimeHandler(value))}
               />
             </label>
             <label>
               End:
               <TimeSelector
+                id = "endTimePicker"
                 TimeValue={props.endTimeValue}
                 timeHandler={props.handleStudySessionEndTime}
+                // timeHandler={() => console.log("end Time")}
               />
             </label>
           </div>
@@ -58,7 +78,7 @@ const AddStudySession = (props) => {
                 onChange={props.FormHandler}
                 name="notes"
                 type="text" 
-                value= {props.entireState.notes}
+                value= {props.entireState.notesValue}
               />
             </label>
           

2fd9cf44d597de43a6418865fc6958ed98864fe8
Chad Archila <chadparchila@gmail.com>
2020-06-19 10:10:46 -0700

    Conver add test component to use redux

--- src/Components/AddTest.js
+++ src/Components/AddTest.js
@@ -2,16 +2,24 @@ import React from 'react';
 import './AddTest.css';
 import Input from './Reusable Components/Input.js';
 import {useDispatch, useSelector} from 'react-redux'
-import {formHandler, showSubmitMessage} from '../actions/index.js'
+import {formHandler, showSubmitMessage, addTest} from '../actions/index.js'
 
 const AddTest = (props) => {
   const dispatch = useDispatch()
   const testNameValue = useSelector(state => state.FormHandlerReducer.testNameToAdd)
+  const selectedClassId = useSelector(state => state.FormHandlerReducer.selectedClass)
+  const allClassIdArr = useSelector(state => state.classesReducer.allID)
+  const allClassObj = useSelector(state => state.classesReducer.byID)
+
+  allClassIdArr.map((classID) => (
+    console.log(allClassObj[classID].classTitle)    
+  ))
 
   const submit = (event, testNameValue) => {
     event.preventDefault();
     console.log("Submit Clicked")
     props.AddTestSubmitBtnHandler(event, testNameValue)
+    dispatch(addTest(selectedClassId, testNameValue))
     dispatch(showSubmitMessage())
   }
 
@@ -30,13 +38,21 @@ const AddTest = (props) => {
           className='addTestForm'
         >
           <label> 
-              <select className='select' name="selectedClass" onChange={props.FormHandler}>
-                <option>Class Name:</option> 
-                {props.entireState.classes.map((classes, idx) => (
+              {/* <select className='select' name="selectedClass" onChange={props.FormHandler}> */}
+              <select className='select' name="selectedClass" onChange={(event) => dispatch(formHandler(event))}>
+                <option>Class Name:</option>
+
+                {allClassIdArr.map((classID) => (
+                  <option key={allClassObj[classID].classTitle} value={allClassObj[classID].id}>
+                    {allClassObj[classID].classTitle}
+                  </option>
+                ))}
+
+                {/* {props.entireState.classes.map((classes, idx) => (
                   <option key={classes.classTitle} value={idx}>
                     {classes.classTitle}
                   </option>
-                ))}
+                ))} */}
               </select>
           </label>
           <br/>
---
+++ src/reducers/testList.js
@@ -0,0 +1,55 @@
+const testList = {
+  "class0": {
+    "test0" : {
+      id: "test0",
+      testTitle: 'Chapter 1'
+    },
+    "test1" : {
+      id: "test1",
+      testTitle: 'Chapter 2'
+    }
+  }
+}
+
+const testReducer = (state = testList, action) => {
+  switch(action.type){
+    case 'ADD_TEST':
+      
+      let testTitleToAdd = action.testTitleToAdd
+      let classIdNumber = action.classIdNumber
+
+      let updatedState = {}
+
+      // copy state immutably
+      for(const key in state){
+        updatedState[key] = {...state[key]}
+        for(const key2 in state[key]){
+            updatedState[key][key2] = {...state[key][key2]}
+        }
+      }
+
+      // if class isn't in the state object, add it.
+      if(!updatedState[classIdNumber]){
+        updatedState[classIdNumber] = {}
+      }
+      debugger
+
+      // create new test ID
+      let newNum = Object.keys(updatedState[classIdNumber]).length
+      let newTestID = "test" + newNum
+
+      // update the copied state object
+      updatedState[classIdNumber][newTestID] = {
+        id: newTestID,
+        testTitle: testTitleToAdd
+      }
+
+      // update state
+      return updatedState;
+
+    default:
+      return state
+  }
+}
+
+export default testReducer

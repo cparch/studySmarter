9c073f19c9f8d8f1d1d382445151da039953da2d
Chad Archila <chadparchila@gmail.com>
2020-08-11 20:34:46 -0700

    Update state structure

--- src/reducers/studySession.js
+++ src/reducers/studySession.js
@@ -2,13 +2,20 @@ const studySessionReducer = (state = {}, action) => {
   switch(action.type){
     case 'ADD_STUDY_SESSION_DETAILS':
 
-      console.log("hello from studySession.js")
-      console.log(action.classID, action.testID, action.notes)
-
       /* 8/8/2020: Logic is here to update the obj. 
       - Now I need to figure out out to copy the state in an immutable fashion
+
+      state: {
+        class0: [
+          {
+            studySession0: {
+              notes: "notes1"
+            }
+          }
+        ]
+      }
       - Add times
-        - there are a few different times that we need for various caluculations. Check the old state for everything we need. 
+        - there are a few different times that we need for various calculations. Check the old state for everything we need. 
         - It might make more sense to figure out that times first. That ways we understand the entire state obj stricture, before we try to copy it in an immutable fashion.
       */
 
@@ -16,12 +23,16 @@ const studySessionReducer = (state = {}, action) => {
       const testName = 'test' + action.testID      
       // if no class, add the class, test, and notes
       if(updatedState['class'+ action.classID] === undefined){
-        updatedState['class'+ action.classID] = {[testName]: [{StudySession0: action.notes}] }
-      } else if (updatedState['class'+ action.classID][testName] === undefined) {
-        updatedState['class'+ action.classID][testName] = [{StudySession0: action.notes}]
-      } else {
+        updatedState['class'+ action.classID] = {[testName]: [{StudySession0: {notes: action.notes}}] }
+      } 
+      // else if no test add the test
+      else if (updatedState['class'+ action.classID][testName] === undefined) {
+        updatedState['class'+ action.classID][testName] = [{StudySession0: {notes: action.notes}}]
+      } 
+      // else add the study session to the test
+      else {
         let selectedTestStudySessionArray = updatedState['class'+ action.classID][testName]
-        selectedTestStudySessionArray.push({['studySession' + selectedTestStudySessionArray.length]: action.notes})
+        selectedTestStudySessionArray.push({['studySession' + selectedTestStudySessionArray.length]: {notes: action.notes}})
       }
 
     

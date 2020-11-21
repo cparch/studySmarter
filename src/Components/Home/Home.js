17acb7834fe83d911bdd8796ed86bd1916ca3992
Chad Archila <chadparchila@gmail.com>
2020-11-21 11:21:26 -0800

    Use homePageReducer

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,15 +1,42 @@
 import React from 'react';
 import ShowTest from './ShowTest.js'
 import './Home.css';
+import { useSelector } from 'react-redux'
+import { useDispatch } from 'react-redux'
+import {toggleShowTests} from '../../actions/index.js'
+
 
 const Home = (props) => {
-  const courseNames = props.classList.map((courseInfo, idx) => {
+  const classList = useSelector(state => state.classesReducer.allID)
+  const classInfo = useSelector(state => state.classesReducer.byID)
+  const showTestList = useSelector(state => state.homePageReducer)
+
+
+  const dispatch = useDispatch()
+
+
+  console.log('classList: ', classList)
+
+//this is what was passed initially
+  console.log("props.classList: ", props.classList)
+  // const courseNames = props.classList.map((courseInfo, idx) => {
+  const courseNames = classList.map((classId, idx) => {
+
+    console.log('classID: ',classId )
+    // console.log('className: ', classInfo[classId].classTitle)
+
+    let classTitle = classInfo[classId].classTitle
+//11/21: create a homepage Handler that keeps track of whats going on in the home page. homePageShowClassInfo... if true show a list of tests for that class. homePageShowTestStudySessions.... if true show the study sessions.   when user pressed submit to add a test, we will also invoke a function that will add to the homePage handler
     
     let displayClassInfo = null
 
-    if(courseInfo.homePageShowClassInfo){
+    // if(courseInfo.homePageShowClassInfo){
+    if(showTestList[classId].ShowTests){
+      // console.log("***** show test list")
       displayClassInfo = <ShowTest
-        courseInfo={courseInfo}
+        // courseInfo={courseInfo}
+        classId={classId}
+
         classIdx={idx}
         homePageShowTestStudySessions={props.homePageShowTestStudySessions}
       />
@@ -18,13 +45,17 @@ const Home = (props) => {
     return(
       <div 
         className='courseContainer'
-        key={courseInfo.classTitle}
+        key={classTitle}
+
       >
         <div
           className='courseInfo' 
-          onClick={() => props.homePageShowClassInfo(idx)}
+          // onClick={() => props.homePageShowClassInfo(idx)}
+          onClick={() =>  dispatch(toggleShowTests(classId))
+          }
         >
-          <div className='courseTitle' >{courseInfo.classTitle} </div>
+          <div className='courseTitle' >{classTitle} </div>
+
         </div>
         <div>
           {displayClassInfo}
--- src/Components/Home/ShowTest.js
+++ src/Components/Home/ShowTest.js
@@ -1,28 +1,45 @@
 import React from 'react';
 import ShowStudySessions from './ShowStudySessions.js';
 import './ShowTest.css';
+import { useSelector } from 'react-redux'
 
 const ShowTests = (props) => {
+  let testList = useSelector(state => state.testReducer)
+  let testNameArray = [];
 
-  let listItems = props.courseInfo.test.map((testInfo, testIdx) =>{
+  // this gathers all the test names for the selected class and puts them in an array
+  for(const classObj in testList){
+    for(const testObj in testList[classObj]){
+      testNameArray.push(testList[classObj][testObj].testTitle)
+    }
+  }
+
+  //11/21: on the home page, when you click the class name, we can now see the test names. The next thing is when you click on the test name we should get all the study session info. The very next task is to have HomePageHandler update showStudySessions for that particular test. I think I need to add the showStudySessions boolen to study session reducer, because we only want to see the study sessions for the test of our choice. 
+
+  // let listItems = props.courseInfo.test.map((testInfo, testIdx) =>{
+  let listItems = testNameArray.map((testName, testIdx) =>{
+
     let showStudySession = null
-    let testGradeOrStudyTime = `Total Time Studied for this test: ${testInfo.totalTimeStudiedPerTest}`
-    if(testInfo.homePageShowStudySessions){
-      showStudySession = <ShowStudySessions
-        studySessions={testInfo.studySession}
-      />
-    }
+    // let testGradeOrStudyTime = `Total Time Studied for this test: ${testInfo.totalTimeStudiedPerTest}`
+    let testGradeOrStudyTime = `Work on line 9 in showTest.js`
 
-    if(testInfo.grade){
-      testGradeOrStudyTime = `Grade: ${testInfo.grade}`
-    }
+    //11/21: work on this once test are displaied. This shows the study session info
+    // if(testInfo.homePageShowStudySessions){
+    //   showStudySession = <ShowStudySessions
+    //     studySessions={testInfo.studySession}
+    //   />
+    // }
+
+    // if(testInfo.grade){
+    //   testGradeOrStudyTime = `Grade: ${testInfo.grade}`
+    // }
     return(
       <div 
         className='testName'
         onClick={() => props.homePageShowTestStudySessions(props.classIdx, testIdx)}
-        key={testInfo.testName}
+        key={testName}
       >
-        {testInfo.testName}: {testGradeOrStudyTime}
+        {testName}: {testGradeOrStudyTime}
         {showStudySession}
       </div> 
     )

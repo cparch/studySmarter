2f10a4847fee24486cd8b68f74a253365ed95870
Chad Archila <chadparchila@gmail.com>
2020-11-28 12:47:54 -0800

    update to get array from redux

--- src/Components/Home/ShowStudySessions.js
+++ src/Components/Home/ShowStudySessions.js
@@ -1,30 +1,40 @@
 import React from 'react';
 import './ShowStudySessions.css'
+import { useSelector } from 'react-redux'
 
 const ShowStudySession = (props) => { 
+  const classId = props.studySessions.classId
+  const testId = props.studySessions.testId
+  const studySessionList = useSelector(state => state.studySessionReducer[classId][testId].studySessionList)
 
-  const listItems = props.studySessions.map(studySession => {
+  console.log('studySessionList: ', studySessionList)
 
+  //11/28: need to figure out how to come up with a function to determine the duration of study time. Check and see how we did it the old way before redux Read note from 11/21 in showTest.js to figure out how to merge back to master.
+
+  const listItems = studySessionList.map((studySessionObj, idx )=> {
+    console.log('line 21: studySession+idx: ', 'studySession'+idx)
+    console.log(studySessionObj['StudySession'+idx].SelectedStartTimeValueToDisplay)
     return(
-      <div className='studySessionContainer'>
-        <div className='sessionNum'>
-          Study Session Number: {studySession.studySessionNum}
+      <div className='studySessionContainer' key={idx} >
+        <div className='sessionNum' >
+          Study Session Number: {idx + 1}
         </div>
+        
         <br/>
         <div>
-          Start Time: {studySession.startTimeToDisplay}
+          Start Time: {studySessionObj['StudySession'+idx].SelectedStartTimeValueToDisplay}
         </div>
 
         <div>
-          End Time: {studySession.endTimeToDisplay}
+          End Time: {studySessionObj['StudySession'+idx].SelectedEndTimeValueToDisplay}
         </div>
 
-        <div>
+        {/* <div>
           Duration of study session: {studySession.studySessionDuration}
-        </div>
+        </div> */}
         <br/>
         <div>
-          Notes: {studySession.notes}
+          Notes: {studySessionObj['StudySession'+idx].notes}
         </div>
       </div>
     )

f0335afd9fcb59be41ca20d0e65f6977ce6e844c
Chad Archila <chadparchila@gmail.com>
2021-01-10 09:19:28 -0800

    Update logic used to display study sessions

--- src/Components/Home/ShowTest.js
+++ src/Components/Home/ShowTest.js
@@ -13,8 +13,6 @@ const ShowTests = (props) => {
   let allStudySessions = useSelector(state => state.studySessionReducer)
   let testNameArray = [];
   let classId = props.classId
-  debugger
-  
 
   // this gathers all the test names for the selected class and puts them in an array
   for(const classObj in testList){
@@ -27,35 +25,34 @@ const ShowTests = (props) => {
     listItems =  <div className='testName'> You have not added any tests. Please add a test.</div> 
   } else {
      listItems = testNameArray.map((testName, testIdx) =>{
-      let selectedTestId = 'test'+testIdx
-
-      //12/19: next we need to check grade addition/update. Display the grade somewhere. And get AVG time studying spent to achieve a grade
-      let convertMinuteToTime = (timeInMinutes) => {
-        let minutes = timeInMinutes % 60;
-        let hours = (timeInMinutes - minutes)/60
-
-        if(minutes < 10){
-          minutes = '0' + minutes
-        }
-
-        return hours + ":" + minutes
-      }
-      
-      let timeInMinutes = allStudySessions[classId][selectedTestId].TotalTimeStudiedForTest
-      let testGradeOrStudyTime = `Total Time Studied for this test: ${convertMinuteToTime(timeInMinutes)}`
+      let selectedTestId = 'test'+testIdx      
       
       let StudySession = null
+      
+      if(Object.keys(allStudySessions).length > 0){
 
-      if(allStudySessions[classId][selectedTestId]){
-        // 11/21: Now add study session, then go to home page. Clicking on the class name will show you the tests. Clicking on the tests will change the showStudySession property of the selected test to true in studysessionHandler.
+        //show study time
+        //12/19: next we need to check grade addition/update. Display the grade somewhere. And get AVG time studying spent to achieve a grade
+        let convertMinuteToTime = (timeInMinutes) => {
+          let minutes = timeInMinutes % 60;
+          let hours = (timeInMinutes - minutes)/60
+
+          if(minutes < 10){
+            minutes = '0' + minutes
+          }
+
+          return hours + ":" + minutes
+        }
+
+      let testGradeOrStudyTime = null
+
+        let timeInMinutes = allStudySessions[classId][selectedTestId].TotalTimeStudiedForTest
+        testGradeOrStudyTime = `Total Time Studied for this test: ${convertMinuteToTime(timeInMinutes)}`
+  
+        //show study session
         StudySession = <ShowStudySessions
           studySessions={{classId: classId, testId: selectedTestId}}
         />
-      
-  
-        // if(testInfo.grade){
-        //   testGradeOrStudyTime = `Grade: ${testInfo.grade}`
-        // }
 
         return(
           <div 
@@ -80,8 +77,6 @@ const ShowTests = (props) => {
       }
     });
   }
-
-
 
   return(
     <div>

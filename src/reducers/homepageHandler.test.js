2f63a705b44b463c2c82b2ec7d4c90cd194f3a1f
Chad Archila <chadparchila@gmail.com>
2021-03-21 09:38:01 -0700

    Add test for homePageHandler reducer

---
+++ src/reducers/homepageHandler.test.js
@@ -0,0 +1,58 @@
+import {toggleShowTests, addNewClassToHomepage} from './../actions/index'
+import homePageReducer from './homePageHandler';
+
+describe('Home Page reducer test', () => {
+  it('should return default state',  () => {
+    const newState = homePageReducer(undefined, {})
+    expect(newState).toEqual({
+      "class0": {
+        ShowTests: false,
+        ShowStudySessions:false
+      },
+      "class1": {
+        ShowTests: false,
+        ShowStudySessions:false
+      } 
+    });
+  })
+  
+  describe('Testing toggleShowTests', () => {
+    it('should return class0 showTest as true',  () => {
+      const newState = homePageReducer(undefined, toggleShowTests("class0"))
+      expect(newState).toEqual({
+        "class0": {
+          ShowTests: true,
+          ShowStudySessions:false
+        },
+        "class1": {
+          ShowTests: false,
+          ShowStudySessions:false
+        } 
+      });
+    })
+  
+  })
+
+  describe('Testing addNewClassToHomepage', () => {
+    it('should return new obj class2',  () => {
+      console.log(addNewClassToHomepage())
+      const newState = homePageReducer(undefined, addNewClassToHomepage())
+      expect(newState).toEqual({
+        "class0": {
+          ShowTests: true,
+          ShowStudySessions:false
+        },
+        "class1": {
+          ShowTests: false,
+          ShowStudySessions:false
+        },
+        "class2": {
+          ShowTests: false,
+          ShowStudySessions:false
+        } 
+      });
+    })
+  
+  })
+
+})

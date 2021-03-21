3bb2fcb63d6e15dc6a9dc94fac0a0ed74854445c
Chad Archila <chadparchila@gmail.com>
2021-03-21 09:35:15 -0700

    Remove Cruft

--- src/reducers/sideDrawer.js
+++ src/reducers/sideDrawer.js
@@ -1,9 +1,6 @@
 const sideDrawerReducer = (state = {showSideDrawer: false}, action) => {
   switch(action.type){
     case 'SIDE_DRAWER_TOGGLE':
-
-      console.log("hello from SIDEDRAWER.js")
-
       const updatedState = {...state}
       updatedState.showSideDrawer = !state.showSideDrawer
       console.log("updated state :", updatedState )

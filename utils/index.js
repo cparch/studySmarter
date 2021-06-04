53cf3647f20c777277bbd7cf842b7ea8dba71f2e
cparch <chadparchila@gmail.com>
2021-06-04 08:39:27 -0700

    Add testStore util function

--- utils/index.js
+++ utils/index.js
@@ -1,7 +1,18 @@
+import { applyMiddleware , createStore } from 'redux'
+import rootReducer from '../src/reducers'
+import { middlewares } from '../src/createStore'
+
 /* check prop types function: https://youtu.be/JQD-ApooNMI?list=PL-Db3tEF6pB8Am-IhCRgyGSxTalkDpUV_&t=1061 */
 
+/* redux connect and mapstate to props: https://youtu.be/eSzRt9fFoiw?t=2084 */
 
 export const findByTestAttr = (component, attr) => {
   const wrapper = component.find(`[data-test='${attr}']`);
   return wrapper
 };
+
+/* https://youtu.be/eSzRt9fFoiw?t=2593 */
+export const testStore = (initialState) => {
+  const createStoreWithMiddleware = applyMiddleware(...middlewares)(createStore)
+  return createStoreWithMiddleware(rootReducer, initialState)
+}

94d3351a7a18e068b75e92f057ff72d46575424b
cparch <chadparchila@gmail.com>
2021-06-04 08:54:44 -0700

    Update class to className and add data-test attr for testing

--- src/Components/Reusable Components/Input.js
+++ src/Components/Reusable Components/Input.js
@@ -3,18 +3,18 @@ import './Input.css';
 
 const Input = (props) => {
   return(
-    <div class="page">
-      <label class="field a-field a-field_a1">
+    <div className="page" data-test="reusable-form">
+      <label className="field a-field a-field_a1">
         <input 
-          class="field__input a-field__input" 
+          className="field__input a-field__input" 
           placeholder={props.placeholder} 
           name={props.name} 
           onChange={props.onChangeFunc}
           value={props.value}
           required
         />
-        <span class="a-field__label-wrap">
-          <span class="a-field__label">{props.label}:</span>
+        <span className="a-field__label-wrap">
+          <span className="a-field__label">{props.label}:</span>
         </span>
     </label>
   </div>
